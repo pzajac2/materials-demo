4a80d369383cc900d186db4a68eeb94ffca049b3
Piotr Zając <pzajac2@gmail.com>
2020-01-27 15:05:02 +0100

    Dodane biblioteki: DoctrineORM oraz DoctrineMigrations

--- config/modules.config.php
+++ config/modules.config.php
@@ -12,7 +12,15 @@
  * This should be an array of module namespaces used in the application.
  */
 return [
+    'Zend\Cache',
+    'Zend\Form',
+    'Zend\InputFilter',
+    'Zend\Filter',
+    'Zend\Paginator',
+    'Zend\Hydrator',
     'Laminas\Router',
     'Laminas\Validator',
+    'DoctrineModule',
+    'DoctrineORMModule',
     'Application',
 ];
