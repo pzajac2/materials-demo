a51e2465a7c34c04a01da6a7be53373d10ed3835
Piotr Zając <pzajac2@gmail.com>
2020-01-28 18:14:49 +0100

    Wydzielenie kod UnitsOfMeasureController do SimpleCrudController

---
+++ module/Application/src/Controller/SimpleCrudController.php
@@ -0,0 +1,185 @@
+<?php
+
+namespace Application\Controller;
+
+use Application\Hydrator\EntityHydrator;
+use Application\Traits\EntityManagerProperty;
+use Doctrine\DBAL\Exception\ForeignKeyConstraintViolationException;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityNotFoundException;
+use Doctrine\ORM\EntityRepository;
+use Doctrine\ORM\OptimisticLockException;
+use Doctrine\ORM\ORMException;
+use Doctrine\ORM\TransactionRequiredException;
+use Laminas\Http\PhpEnvironment\Request;
+use Laminas\Http\PhpEnvironment\Response;
+use Laminas\Mvc\Controller\AbstractActionController;
+use Laminas\View\Model\ViewModel;
+use Zend\Form\Form;
+
+/**
+ * Class SimpleCrudController
+ * Kontroler odpowiada za podstawowe operacje na encjach:
+ *  dodanie, edycja, skasowanie, listowanie
+ *
+ * @package Application\Controller
+ * @method Request getRequest()
+ * @method Response getResponse()()
+ */
+abstract class SimpleCrudController extends AbstractActionController
+{
+    use EntityManagerProperty;
+
+    /**
+     * @return string zwraca nazwę klasy encji (FQDN)
+     */
+    abstract protected function getEntityClass(): string;
+
+    /**
+     * Zwraca formularz odpowiedzialny za encję
+     * @return Form
+     */
+    abstract protected function getForm(): Form;
+
+    /**
+     * SimpleCrudController constructor.
+     * @param EntityManager $entityManager
+     */
+    public function __construct(EntityManager $entityManager)
+    {
+        $this->setEntityManager($entityManager);
+    }
+
+    /**
+     * Index - lista encji
+     * @return ViewModel
+     */
+    public function indexAction()
+    {
+        /** @var EntityRepository $repository */
+        $repository = $this->getEntityManager()->getRepository($this->getEntityClass());
+
+        $entities = $repository->findAll();
+
+        return new ViewModel([
+            'entities' => $entities
+        ]);
+    }
+
+    /**
+     * Add - dodanie nowej encji
+     *
+     * @return Response|ViewModel
+     * @throws EntityNotFoundException
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function addAction()
+    {
+        return $this->editAction();
+    }
+
+    /**
+     * Edit - edycja istniejącej encji
+     * Ta sama metoda obsługuje akcję dodania (add)
+     *
+     * @return \Laminas\Http\Response|ViewModel
+     * @throws EntityNotFoundException
+     * @throws ORMException
+     * @throws OptimisticLockException
+     */
+    public function editAction()
+    {
+        $entityClass = $this->getEntityClass();
+
+        /** @var EntityRepository $repository */
+        $repository = $this->getEntityManager()->getRepository($entityClass);
+
+        $id = $this->getEvent()->getRouteMatch()->getParam('id', null);
+        $newEntity = ($id === null);
+
+        $form = $this->getForm();
+        $hydrator = new EntityHydrator($this->getEntityManager());
+
+        $entity = new $entityClass();
+        if (!$newEntity) {
+            $entity = $repository->find($id);
+            if (!$entity) {
+                throw new EntityNotFoundException(sprintf(
+                    'Entity %s, id=%d not found',
+                    $entityClass,
+                    $id
+                ));
+            }
+
+            $data = $hydrator->extract($entity);
+            $form->setData($data);
+        }
+
+        $viewModel = (new ViewModel())
+            ->setVariables([
+                'form' => $form,
+                'newEntity' => $newEntity
+            ])
+            ->setTemplate(static::TEMPLATE_EDIT)
+        ;
+
+        if ($this->getRequest()->isPost()) {
+            $form->setData($this->getRequest()->getPost());
+
+            $isValid = $form->isValid();
+            $data = $form->getData();
+            $form->setData($data);
+
+            if ($isValid) {
+                $hydrator->hydrate($data, $entity);
+
+                $this->getEntityManager()->persist($entity);
+                $this->getEntityManager()->flush();
+
+                return $this->redirect()->toRoute(static::ROUTE_NAME);
+            }
+        }
+
+        return $viewModel;
+
+    }
+
+    /**
+     * Delete - kasowanie encji
+     *
+     * @return \Laminas\Http\Response
+     * @throws EntityNotFoundException
+     * @throws ORMException
+     * @throws OptimisticLockException
+     * @throws TransactionRequiredException
+     *
+     * @todo Possible XSS - request should be validated
+     */
+    public function deleteAction()
+    {
+        $id = $this->getEvent()->getRouteMatch()->getParam('id', null);
+        if (!$id) {
+            throw new \InvalidArgumentException('Id argument missing');
+        }
+
+        $entity = $this->getEntityManager()->find($this->getEntityClass(), $id);
+
+        if (!$entity) {
+            throw new EntityNotFoundException(sprintf(
+                'Entity %s, id=%d not found',
+                $this->getEntityClass(),
+                $id
+            ));
+        }
+
+        try {
+            $this->getEntityManager()->remove($entity);
+            $this->getEntityManager()->flush();
+        } catch (ForeignKeyConstraintViolationException $e) {
+            // nie można skasować - rekord jest używany
+        }
+
+        return $this->redirect()->toRoute(static::ROUTE_NAME);
+    }
+}
--- module/Application/src/Controller/UnitsOfMeasureController.php
+++ module/Application/src/Controller/UnitsOfMeasureController.php
@@ -6,118 +6,32 @@ namespace Application\Controller;
 
 use Application\Entity\UnitOfMeasure;
 use Application\Form\UnitOfMeasureForm;
-use Application\Hydrator\EntityHydrator;
-use Application\Traits\EntityManagerProperty;
-use Doctrine\DBAL\Exception\ForeignKeyConstraintViolationException;
-use Doctrine\ORM\EntityManager;
-use Doctrine\ORM\EntityNotFoundException;
-use Doctrine\ORM\EntityRepository;
-use Laminas\Http\PhpEnvironment\Request;
-use Laminas\Http\PhpEnvironment\Response;
-use Laminas\Mvc\Controller\AbstractActionController;
-use Laminas\View\Model\ViewModel;
+use Zend\Form\Form;
 
 /**
  * Class UnitsOfMeasureController
  * @package Application\Controller
- * @method Request getRequest()
- * @method Response getResponse()()
  */
-class UnitsOfMeasureController extends AbstractActionController
+class UnitsOfMeasureController extends SimpleCrudController
 {
-    use EntityManagerProperty;
-
     const ROUTE_NAME = 'units_of_measure';
+    const TEMPLATE_INDEX = 'application/units-of-measure/index';
     const TEMPLATE_EDIT = 'application/units-of-measure/edit';
 
-    public function __construct(EntityManager $entityManager)
+    /**
+     * @return string
+     */
+    protected function getEntityClass(): string
     {
-        $this->setEntityManager($entityManager);
+        return UnitOfMeasure::class;
     }
 
-    protected function getRepository(): EntityRepository
+    /**
+     * @return Form
+     */
+    protected function getForm(): Form
     {
-        return $this->getEntityManager()->getRepository(UnitOfMeasure::class);
+        return new UnitOfMeasureForm('unit');
     }
 
-    public function indexAction()
-    {
-        $units = $this->getRepository()->findAll();
-
-        return new ViewModel([
-            'units' => $units
-        ]);
-    }
-
-    public function addAction()
-    {
-        return $this->editAction();
-    }
-
-    public function editAction()
-    {
-        $id = $this->getEvent()->getRouteMatch()->getParam('id', null);
-        $newEntity = ($id === null);
-
-        $form = new UnitOfMeasureForm('unit');
-        $hydrator = new EntityHydrator($this->getEntityManager());
-
-        $entity = new UnitOfMeasure();
-        if (!$newEntity) {
-            $entity = $this->getRepository()->find($id);
-            if (!$entity) {
-                throw new EntityNotFoundException(sprintf('Unit of measure with id=%d not found', $id));
-            }
-
-            $data = $hydrator->extract($entity);
-            $form->setData($data);
-        }
-
-        $viewModel = (new ViewModel())
-            ->setVariables([
-                'form' => $form,
-                'newEntity' => $newEntity
-            ])
-            ->setTemplate(self::TEMPLATE_EDIT)
-        ;
-
-        if ($this->getRequest()->isPost()) {
-            $form->setData($this->getRequest()->getPost());
-
-            $isValid = $form->isValid();
-            $data = $form->getData();
-            $form->setData($data);
-
-            if ($isValid) {
-                $hydrator->hydrate($data, $entity);
-
-                $this->getEntityManager()->persist($entity);
-                $this->getEntityManager()->flush();
-
-                return $this->redirect()->toRoute(self::ROUTE_NAME);
-            }
-        }
-
-        return $viewModel;
-
-    }
-
-    public function deleteAction()
-    {
-        $id = $this->getEvent()->getRouteMatch()->getParam('id', null);
-
-        $entity = $this->getRepository()->find($id);
-        if (!$entity) {
-            throw new EntityNotFoundException(sprintf('Unit of measure with id=%d not found', $id));
-        }
-
-        try {
-            $this->getEntityManager()->remove($entity);
-            $this->getEntityManager()->flush();
-        } catch (ForeignKeyConstraintViolationException $e) {
-            // nie można skasować - rekord jest używany
-        }
-
-        return $this->redirect()->toRoute(self::ROUTE_NAME);
-    }
 }
